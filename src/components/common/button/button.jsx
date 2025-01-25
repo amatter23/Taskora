deb5c375612af5b89d1e248e9132690e4d30c694
amatter23 <matterahmed36@gmail.com>
2025-01-25 12:12:56 +0200

    enhance button component with dynamic styles and active state management

--- src/components/common/button/button.jsx
+++ src/components/common/button/button.jsx
@@ -1,11 +1,20 @@
 import style from './button.module.css';
-const Button = ({ children, onClick }) => {
-  console.log('but run');
+const Button = ({ children, onClick, isActive, bgColor, textColor }) => {
   const handleClick = e => {
     onClick(e);
   };
+
+  const buttonClasses = [
+    style.button,
+    isActive && style['buttonActive'],
+    bgColor && style[`buttonBg`],
+    textColor && style[`buttonText`],
+  ]
+    .filter(Boolean)
+    .join(' ');
+
   return (
-    <div onClick={handleClick} className={style.container}>
+    <div onClick={handleClick} className={buttonClasses}>
       <div className={style.content}>{children}</div>
     </div>
   );
