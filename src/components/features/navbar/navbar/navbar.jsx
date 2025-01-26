d092ff8706d72337faecda03fda87badf357a9bd
amatter23 <matterahmed36@gmail.com>
2025-01-26 16:06:21 +0200

    add ChangeThemeButton to Navbar for theme switching functionality

--- src/components/features/navbar/navbar/navbar.jsx
+++ src/components/features/navbar/navbar/navbar.jsx
@@ -1,13 +1,20 @@
 import style from './navbar.module.css';
 import Logo from '../logo/logo';
 import Image from '../image/image';
+import ChangeThemeButton from '../../../common/changeThemeButton/changeThemeButton';
 const Navbar = () => {
   return (
     <nav>
       <div className={style.logo}>
         <Logo></Logo>
       </div>
-      <div className={style.acions}>
+      <div className={style.actions}>
+        <div className={style.changeTheme}>
+          <ChangeThemeButton
+            size={'calc(var(--current-font-size-sm)*2.5)'}
+          ></ChangeThemeButton>
+        </div>
+
         <Image></Image>
       </div>
     </nav>
