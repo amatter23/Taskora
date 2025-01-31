63240da67052a3a78da24dd7b757be0a2e992c89
amatter23 <matterahmed36@gmail.com>
2025-01-31 03:21:28 +0200

    add Header component: enhance functionality with add button and callback for dynamic content display

--- src/components/features/SidebarMenu/header/header.jsx
+++ src/components/features/SidebarMenu/header/header.jsx
@@ -1,11 +1,23 @@
+/**
+ * Header component for displaying a section title with an add button
+ * @component
+ * @param {Object} props - Component props
+ * @param {string} props.type - The type of content being displayed (e.g., "tasks", "projects")
+ * @param {Function} props.onClick - Callback function triggered when the add button is clicked
+ * @returns {JSX.Element} Header component with title and add button
+ */
 import style from './header.module.css';
 import Button from '../../../common/button/button';
 import { MdAddCircleOutline } from 'react-icons/md';
-const Header = ({ type }) => {
+const Header = ({ type, onClick }) => {
   return (
     <div className={style.header}>
       <h4>Recent {type}</h4>
-      <Button bgColor={true} color={'var(--light-text-color)'}>
+      <Button
+        onClick={onClick}
+        bgColor={true}
+        color={'var(--light-text-color)'}
+      >
         <MdAddCircleOutline />
       </Button>
     </div>
