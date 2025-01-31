0d5ee78039abec0e2272ac081b090effb654f1ce
amatter23 <matterahmed36@gmail.com>
2025-01-31 03:23:32 +0200

    add App component: implement loading state and layout structure with Dashboard and Modal integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
-function App() {
-  return <></>;
-}
+import { useState, useEffect } from 'react';
+import Loading from './components/common/loading /loading';
+import Dashboard from './components/features/global/dashboard/dashboard';
+import Modal from './components/common/modal/modal';
+import useFetchAllData from './hooks/useFetchAllData';
+import MainLayout from './components/layouts/mainLayout/mainLayout';
+import { useTheme } from './hooks/useTheme';
 
+function App() {
+  const { theme } = useTheme();
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+  }, [theme]);
+
+  const loading = useFetchAllData();
+  if (loading) return <Loading text='Creating your workspace magic'></Loading>;
+  return (
+    <>
+      <MainLayout>
+        <Dashboard />
+      </MainLayout>
+      <Modal></Modal>
+    </>
+  );
+}
 export default App;
