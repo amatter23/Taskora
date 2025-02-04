84c441eb08bfa3c785241a064e8825329e291638
amatter23 <matterahmed36@gmail.com>
2025-02-04 02:16:21 +0200

    feat: implement search and filter functionality in Dashboard component with keyboard shortcuts

---
+++ src/components/features/global/filter/filter/filter.jsx
@@ -0,0 +1,34 @@
+import DropDown from '../../view/dropDown/dropDown';
+import style from './filter.module.css';
+import { useState } from 'react';
+import { FaFilter } from 'react-icons/fa6';
+import FilterPicker from '../filterPicker/filterPicker';
+import useHandleNames from '../../../../../hooks/useHandleNames';
+const Filter = ({ searchData }) => {
+  const handelNames = useHandleNames();
+  const [search, setSearch] = useState();
+  const handelSearchData = e => {
+    searchData(e);
+    setSearch(e);
+  };
+
+  return (
+    <>
+      <DropDown
+        onChange={handelSearchData}
+        content={FilterPicker}
+        placement={'bottom'}
+      >
+        <div className={style.filter}>
+          <div>
+            {search?.status?.name
+              ? handelNames(search?.status?.name)
+              : 'Filter'}
+          </div>
+          <FaFilter color={search?.status?.color} />
+        </div>
+      </DropDown>
+    </>
+  );
+};
+export default Filter;
---
+++ src/components/features/global/filter/filterPicker/filterPicker.jsx
@@ -0,0 +1,143 @@
+import { useSelector } from 'react-redux';
+import { useState, useRef } from 'react';
+import style from './filterPicker.module.css';
+import { selectAllStatuses } from '../../../../../store/selectors/statuses/allStatusesSelector';
+import useHandleNames from '../../../../../hooks/useHandleNames';
+
+const FilterPicker = ({ onChange }) => {
+  const [search, setSearch] = useState({
+    date: '',
+    status: '',
+  });
+  const statuses = useSelector(selectAllStatuses);
+  const handelName = useHandleNames();
+  return (
+    <div className={style.filterContainer}>
+      <div className={style.filterFields}>
+        <div className={style.filterSection}>
+          <h4>status</h4>
+          <div className={style.statusGrid}>
+            {statuses.map(status => (
+              <div
+                onClick={() => {
+                  onChange({
+                    ...search,
+                    status: status,
+                  });
+                  setSearch({
+                    ...search,
+                    status: status,
+                  });
+                }}
+                key={status.name}
+                className={
+                  search?.status?.name === status.name
+                    ? style.active
+                    : style.statusOption
+                }
+              >
+                <p>{handelName(status.name)}</p>
+                <div
+                  style={{ backgroundColor: status.color }}
+                  className={style.statusIndicator}
+                ></div>
+              </div>
+            ))}
+            <div
+              onClick={() => {
+                onChange({
+                  ...search,
+                  status: '',
+                });
+                setSearch({
+                  ...search,
+                  status: '',
+                });
+              }}
+              key={''}
+              className={
+                search.status === '' ? style.active : style.statusOption
+              }
+            >
+              <p>All</p>
+              <div className={style.statusIndicator}></div>
+            </div>
+          </div>
+        </div>
+        <div className={style.filterSection}>
+          <h4>Date</h4>
+          <div className={style.dateGrid}>
+            <p
+              onClick={() => {
+                onChange({
+                  ...search,
+                  date: 'today',
+                });
+                setSearch({
+                  ...search,
+                  date: 'today',
+                });
+              }}
+              className={
+                search.date === 'today' ? style.active : style.dateOption
+              }
+            >
+              Today
+            </p>
+            <p
+              onClick={() => {
+                onChange({
+                  ...search,
+                  date: 'week',
+                });
+                setSearch({
+                  ...search,
+                  date: 'week',
+                });
+              }}
+              className={
+                search.date === 'week' ? style.active : style.dateOption
+              }
+            >
+              This Week
+            </p>
+            <p
+              onClick={() => {
+                onChange({
+                  ...search,
+                  date: 'month',
+                });
+                setSearch({
+                  ...search,
+                  date: 'month',
+                });
+              }}
+              className={
+                search.date === 'month' ? style.active : style.dateOption
+              }
+            >
+              This Month
+            </p>
+            <p
+              onClick={() => {
+                onChange({
+                  ...search,
+                  date: '',
+                });
+                setSearch({
+                  ...search,
+                  date: '',
+                });
+              }}
+              className={search.date === '' ? style.active : style.dateOption}
+            >
+              All
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FilterPicker;
