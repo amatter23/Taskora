cb8028b5490fcfffaec00ea27ef856412a17757e
amatter23 <matterahmed36@gmail.com>
2025-02-10 00:38:44 +0200

    feat: remove redundant comment for tag invalidation in projectsApi

--- src/store/services/projectsApi.js
+++ src/store/services/projectsApi.js
@@ -35,7 +35,6 @@ export const projectsApi = createApi({
       async onQueryStarted(id, { dispatch, queryFulfilled }) {
         try {
           await queryFulfilled;
-          // Invalidate Tasks tags
           dispatch(tasksApi.util.invalidateTags(['Tasks']));
         } catch (error) {
           console.error('Failed to invalidate tags:', error);
