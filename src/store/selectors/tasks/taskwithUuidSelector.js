62033ea4775b0a997b70e2dcb61510d119f9841c
amatter23 <matterahmed36@gmail.com>
2025-02-11 16:40:47 +0200

    fix: handle potential undefined tasks in selectTaskWithUuid selector

--- src/store/selectors/tasks/taskwithUuidSelector.js
+++ src/store/selectors/tasks/taskwithUuidSelector.js
@@ -4,6 +4,6 @@ const selectTasks = state => selectTasksFullData(state);
 export const selectTaskWithUuid = createSelector(
   [selectTasks, (state, uuid) => uuid],
   (tasks, uuid) => {
-    return tasks.find(task => task?.uuid === uuid);
+    return tasks?.find(task => task?.uuid === uuid);
   }
 );
