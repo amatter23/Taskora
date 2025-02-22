a652ee02c9fc35f6f34bd951f55ea1fbb88ff7ad
amatter23 <matterahmed36@gmail.com>
2025-02-22 22:50:57 +0200

    feat: update image sources to use local assets for logo and background

--- src/components/features/auth/backgroundImage/backgroundImage.jsx
+++ src/components/features/auth/backgroundImage/backgroundImage.jsx
@@ -4,7 +4,7 @@ const BackgroundImage = () => {
   return (
     <div className={style.picture}>
       <img
-        src='https://res.cloudinary.com/de9jdl1lm/image/upload/v1738898512/taskora-api/ygskxcthhtgrtekq0agd.png'
+        src='/auth.png'
         alt='Background image'
       />
     </div>
--- src/components/features/navbar/logo/logo.jsx
+++ src/components/features/navbar/logo/logo.jsx
@@ -1,11 +1,9 @@
 import style from './logo.module.css';
-import { LuListTodo } from 'react-icons/lu';
-
 const Logo = () => {
   return (
     <div className={style.logo}>
       <img
-        src='https://res.cloudinary.com/de9jdl1lm/image/upload/v1738898452/taskora-api/xfujhxusx4ymtdlclcp3.png'
+        src='/logo.png'
         style={{
           width: '1.5rem',
           height: '1.5rem',
